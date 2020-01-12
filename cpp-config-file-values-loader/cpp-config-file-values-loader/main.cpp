4dc9d95187e2dd6275ec950fcfc3ec59e717dd10
Lucas Charbonnier <lcs.charbonnier@gmail.com>
2020-01-12 17:10:46 +0100

    Loader now checks if every expected identifier has been loaded, simplified the use of the loader and renamed a few things

--- cpp-config-file-values-loader/cpp-config-file-values-loader/main.cpp
+++ cpp-config-file-values-loader/cpp-config-file-values-loader/main.cpp
@@ -3,28 +3,42 @@
 #include "ValuesLoader.h"
 
 int main() {
-	std::vector<ValueFormat> values = {
-		{"real", ValType::Float},
-		{"boolean", ValType::Boolean},
-		{"str", ValType::String},
-		{"integer", ValType::Integer}
+	std::vector<ValFormat> values = {
+		{"real1", ValType::Float},
+		{"real2", ValType::Float},
+		{"real3", ValType::Float},
+		{"invalid_real", ValType::Float},
+
+		{"boolean1", ValType::Boolean},
+		{"boolean2", ValType::Boolean},
+		{"boolean3", ValType::Boolean},
+		{"boolean4", ValType::Boolean},
+		{"invalid_boolean", ValType::Boolean},
+
+		{"integer1", ValType::Integer},
+		{"integer2", ValType::Integer},
+		{"integer3", ValType::Integer},
+		{"invalid_integer", ValType::Integer},
+
+		{"str1", ValType::String},
+		{"str2", ValType::String},
+		{"str3", ValType::String},
+		{"str4", ValType::String},
+		{"str5", ValType::String},
+		{"empty_string", ValType::String},
+
+		{"not_found", ValType::Boolean}
 	};
 
-	ValuesLoader loader(values);
-	loader.loadValuesFromFile("values.txt");
-	if(!loader.everythingIsFine()) {
-		auto errors = loader.getErrors();
+	ValuesLoader loader(values, "values.txt");
+	if(!loader.load()) {
+		auto errors = loader.getErrorList();
 		for (const auto& error : errors) {
 			std::cout << error << std::endl;
 		}
-
-		return EXIT_FAILURE;
 	}
 
-	float flt = loader.getValue<float>("real");
-	bool ok = loader.getValue<bool>("boolean");
-	int integer = loader.getValue<int>("integer");
-	std::string str = loader.getValue<std::string>("str");
+	std::string string = loader.getValue<std::string>("empty_string");
 
 	return EXIT_SUCCESS;
 }
